7b28ca3a18f193a1ce4bf0a9af7db2e4494caf04
Programmer General <programming.democracy@gmail.com>
2017-01-27 22:02:17 -0800

    Adding initial sharing dialog

--- app.js
+++ app.js
@@ -3,9 +3,27 @@ module.config(function($mdThemingProvider) {
   $mdThemingProvider.theme('default').accentPalette('red');
 });
 
-var controller = function() {
+
+var ShareController = function() {
+  this.entries = data;
+};
+module.controller('ShareController', controller);
+
+var controller = function($mdDialog) {
   this.actions = data;
   this.actions.forEach((action) => action.open = false);
+
+  this.share = function(ev) {
+    $mdDialog.show({
+      controller: ShareController,
+      controllerAs: 'dialogCtrl',
+      templateUrl: 'share.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+    });
+  };
+
 };
 
 module.controller('TrumpActionsController', controller);
